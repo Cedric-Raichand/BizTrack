dc728f43e945e6608f44c4826df8a2106136de90
DZODZODZI CEDRICK <cdzodzodzi@st.ug.edu.gh>
2026-07-17 01:17:50 -0930

    implement user registration with password hashing

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,58 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+
 const registerUser = async (req, res) => {
-  res.json({
-    message: "Register user route",
-  });
+  try {
+    const { name, email, password } = req.body;
+
+    // Check if user exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+
+    // Create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
+
 
 const loginUser = async (req, res) => {
   res.json({
     message: "Login user route",
   });
 };
+
 
 module.exports = {
   registerUser,
