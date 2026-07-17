726f810aaeef351cc9d8ba6813d730238373a2b8
DZODZODZI CEDRICK <cdzodzodzi@st.ug.edu.gh>
2026-07-17 01:25:59 -0930

    implement user login with JWT authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 
 const registerUser = async (req, res) => {
@@ -48,9 +49,59 @@ const registerUser = async (req, res) => {
 
 
 const loginUser = async (req, res) => {
-  res.json({
-    message: "Login user route",
-  });
+  try {
+    const { email, password } = req.body;
+
+
+    // Find user
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+
+    // Create JWT token
+    const token = jwt.sign(
+      {
+        id: user._id,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+
+    res.json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
 
