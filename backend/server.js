6bf14d3f945b65b5c0ed6990dbbcb6591d1a5d20
DZODZODZI CEDRICK <cdzodzodzi@st.ug.edu.gh>
2026-07-18 02:09:25 -0930

    connect frontend registration with backend API

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,40 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
+
 const connectDB = require("./config/db");
+
 const userRoutes = require("./routes/userRoutes");
 const businessRoutes = require("./routes/businessRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
+const authRoutes = require("./routes/authRoutes");
+
 
 dotenv.config();
+
 connectDB();
 
 const app = express();
 
+
+// Middleware
+app.use(cors());
 app.use(express.json());
 
-const authRoutes = require("./routes/authRoutes");
+
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/business", businessRoutes);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 
+
 app.get("/", (req, res) => {
   res.send("BizTrack API is running...");
 });
+
 
 const PORT = process.env.PORT || 5000;
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,86 @@
+import { useState } from "react";
+import API from "../api/axios";
+
+
 function Register() {
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      const response = await API.post(
+        "/auth/register",
+        formData
+      );
+
+      console.log(response.data);
+
+      alert("Registration successful");
+
+    } catch(error) {
+
+      console.log(error.response?.data);
+
+      alert("Registration failed");
+
+    }
+  };
+
+
   return (
     <div>
-      <h1>Register Page</h1>
-      <p>Create your BizTrack account</p>
+
+      <h1>Create Account</h1>
+
+      <form onSubmit={handleSubmit}>
+
+        <input
+          name="name"
+          placeholder="Name"
+          onChange={handleChange}
+        />
+
+
+        <input
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
+
+
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+
+
+        <button>
+          Register
+        </button>
+
+      </form>
+
     </div>
   );
 }
+
 
 export default Register;
