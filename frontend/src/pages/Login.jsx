cfeaf0521aa2b10e37eff9b5652b13aa30c6867d
DZODZODZI CEDRICK <cdzodzodzi@st.ug.edu.gh>
2026-07-18 02:17:30 -0930

    implement login authentication with JWT

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,90 @@
+import { useState } from "react";
+import API from "../api/axios";
+
+
 function Login() {
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      const response = await API.post(
+        "/auth/login",
+        formData
+      );
+
+
+      console.log(response.data);
+
+
+      // store token
+      localStorage.setItem(
+        "token",
+        response.data.token
+      );
+
+
+      alert("Login successful");
+
+
+    } catch(error) {
+
+      console.log(error.response?.data);
+
+      alert("Login failed");
+
+    }
+  };
+
+
   return (
     <div>
-      <h1>Login Page</h1>
-      <p>Welcome back to BizTrack</p>
+
+      <h1>Login</h1>
+
+
+      <form onSubmit={handleSubmit}>
+
+        <input
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
+
+
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+
+
+        <button>
+          Login
+        </button>
+
+
+      </form>
+
     </div>
   );
 }
+
 
 export default Login;
