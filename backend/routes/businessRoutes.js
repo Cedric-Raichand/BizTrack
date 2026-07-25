5b67ec4cb153ec24dcbdda79b9a2c062d4d7a151
DZODZODZI CEDRICK <cdzodzodzi@st.ug.edu.gh>
2026-07-25 02:04:07 -0930

    add get all businesses API endpoint

--- backend/routes/businessRoutes.js
+++ backend/routes/businessRoutes.js
@@ -1,15 +1,49 @@
 const express = require("express");
+
 const router = express.Router();
+
 
 const {
   createBusiness,
   getBusiness,
+  getBusinesses,
 } = require("../controllers/businessController");
+
 
 const protect = require("../middleware/authMiddleware");
 
-router.post("/", protect, createBusiness);
 
-router.get("/", protect, getBusiness);
+
+// Create business
+
+router.post(
+  "/",
+  protect,
+  createBusiness
+);
+
+
+
+
+// Get all businesses
+
+router.get(
+  "/all",
+  protect,
+  getBusinesses
+);
+
+
+
+
+// Get single/default business (keep for now)
+
+router.get(
+  "/",
+  protect,
+  getBusiness
+);
+
+
 
 module.exports = router;
