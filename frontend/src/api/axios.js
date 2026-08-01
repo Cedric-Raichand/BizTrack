fa645354b1ea49440ceecf5a7f811aa07d71e02a
DZODZODZI CEDRICK <cdzodzodzi@st.ug.edu.gh>
2026-08-01 01:42:41 +0000

    Fix axios configuration for deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,23 +1,6 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-});
-
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
-
-export default API;
-import axios from "axios";
-
-const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
